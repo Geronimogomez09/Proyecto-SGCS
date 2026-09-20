fcc9afda53244bac8b419d2f32d4dda835f35d28
IvanBarrera512 <ivanesc512@gmail.com>
2026-09-20 18:03:26 -0300

    Agregando bootstrap + css a cards

--- main.js
+++ main.js
@@ -3,9 +3,9 @@
 function cargarComponente(idContenedor, archivoHtml) {
     //hacemos una solicitud fetch para obtener el contenido del archivo HTML
     fetch(archivoHtml)
-        //si la solicitud es exitosa, parseamos el contenido como texto
-        .then(response => {
-            //con .ok verificamos que la respuesta fue exitosa o no (true o fakse).
+    //si la solicitud es exitosa, parseamos el contenido como texto
+        .then((response) => {
+      //con .ok verificamos que la respuesta fue exitosa o no (true o fakse).
             if (!response.ok) {
                 throw new Error(`No se pudo cargar el archivo ${archivoHtml}`);
             }
@@ -13,12 +13,10 @@ function cargarComponente(idContenedor, archivoHtml) {
             return response.text();
         })
         //se recibe el texto plano del archivo HTML y se inserta en el contenedor especificado por su ID
-        .then(data => {
-            document.getElementById(idContenedor).innerHTML = data;
-        })
+        .then((data) => {document.getElementById(idContenedor).innerHTML = data;})
         // si la solicitud falla, se ejecuta este código para mostrar el error en la consola
-        .catch(error => console.error('Error:', error));
+        .catch((error) => console.error("Error:", error));
 }
 
 // Llamamos a la función para cada componente que necesitemos
-cargarComponente('header-container', 'componentes/header.html');
+cargarComponente("header-container", "componentes/header.html");
